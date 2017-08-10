71bce6e5ed31af09600d8badf41b62a39ab90d27
sdinay <sdinay@yahoo.com>
2017-08-09 17:21:23 -0700

    Fixed the zero I avoided

--- Calculator.java
+++ Calculator.java
@@ -10,44 +10,69 @@ class Calculator {
 	public static void main(String[] args) {
 		printGreeting();
 		
+		// Holds the line typed as a string
 		String input;
+		char[] symbols = {'+', '-', '*', '/', '%'};
+		String beforeTheSpace = "nothing";
 
 		while(true) {
 			// Reset Variables
-			int resultLength = 101;
-			int result[] = new int[resultLength];
-			char operations[] = new char[100];
-			int iresu = 0;
-			int ioper = 0;
-			Boolean expressionEntered = false;
+			int resultLength = 101; // Amount of numbers the expression can hold
+			double[] result = new double[resultLength]; // Initially holds the inputed numbers, then holds the results
+			char[] operations = new char[100]; // Holds the operations in order
+			int iresu = 0; // Index for result[]
+			int ioper = 0; // Index for operations[]
+			Boolean expressionEntered = false; // Checks if an expression was entered
 
 			// Scanner Variable
 			Scanner sc = new Scanner(System.in);
+			print("> ");
 			input = sc.nextLine();
 			
+			// typing quit by itself exits while loop and the program
 			if(input.equals("quit")) {
 				break;
 			}
 
+			// typing help outputs usage information
 			if(input.equals("help")) {
 				printHelpInformation();
 			}
 
 			for(int i = 0; i < input.length(); i++) {
-				char a = input.charAt(i);
-				if (a == '+') {
-					operations[ioper] = '+';
+				char a = input.charAt(i); // grab the char
+				// addtion, subtraction
+				if (a == '+' || a == '-' || a == '*' || a == '/') {
+					if(!c_contains(symbols, a)) {
+						println("Warning: symbol not present in calculator");
+						break;
+					}
+					operations[ioper] = a;
 					iresu++;
 					ioper++;
-				} else if (a == '-') {
-					operations[ioper] = '-';
-					iresu++;
-					ioper++;
-				} else if (a == ' ') {
-					iresu++;
-				} else if (a >= 48 || a <= 57) {
-					if( !Character.isDigit(a) )
+				} 
+				//skip the spaces
+				else if (a == ' ') {
+					println("In here: iresu = " + iresu + " ioper = " + ioper);
+					// if it is a number and then another number
+					/*if(iresu == ioper) {
+						// error
+						beforeTheSpace = "number";
+					}*/
+					// if it is a number and then a symbol
+					// good
+					// if it is a symbol and then a number
+					// good
+					// if it is a symbol and then another symbol
+					// error
+				}
+				// number
+				else if (a >= 48 || a <= 57) {
+					// If a is not a digit, skip the char
+					if( !Character.isDigit(a) ) {
+						println("Warning: symbol not present in calculator");
 						continue;
+					}
 					expressionEntered = true;
 					if(result[iresu] == 0) {
 						result[iresu] = a - '0';
@@ -55,20 +80,29 @@ class Calculator {
 						result[iresu] = appendOnesDigit(result[iresu], a-'0');
 					}
 				} else {
-					println("Unknown input");
+					println("Warning: symbol not present in calculator");
 				}
 			}
 
-			int j = 0;
-			int k = 0;
+			int j = 0; // Tracks the result array
+			int k = 0; // Tracks the operations array
 
-			while(j != resultLength-1 && result[j] != 0 && result[j+1] != 0) {
-				if(operations[k] == '+') {
+			println("iresu = " + iresu);
+			println("ioper = " + ioper);
+
+			// If result index is not bigger than result[] and the elements are not empty
+			while(j != resultLength-1 && j < iresu) {//result[j] != 0 && result[j+1] != 0) {
+				println("j = " + j + " and k = " + k);
+				if(operations[k] == '+')
 					result[j+1] = add(result[j], result[j+1]);
-				}
-				if(operations[k] == '-') {
+				if(operations[k] == '-')
 					result[j+1] = subt(result[j], result[j+1]);
-				}
+				if(operations[k] == '*')
+					result[j+1] = mult(result[j], result[j+1]);
+				if(operations[k] == '/')
+					result[j+1] = div(result[j], result[j+1]);
+				if(operations[k] == '%')
+					result[j+1] = modulus(result[j], result[j+1]);
 				j++;
 				k++;
 			}
@@ -78,6 +112,7 @@ class Calculator {
 	}
 
 	// ============= Useful Functions ============= //
+	
 	static void println(Object a) {
     	System.out.println(a);
 	}
@@ -86,25 +121,48 @@ class Calculator {
 		System.out.print(a);
 	}
 
-	static int appendOnesDigit(int a, int ones) {
+	static boolean c_contains(char[] a, char b) {
+		for(int i = 0; i < a.length; i++) {
+			if(a[i] == b) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// Multiplies a number by 10 and addeds a digit 0-9 to it
+	static double appendOnesDigit(double a, double ones) {
 		return (a * 10) + ones;
 	}
 
-	static int add(int a, int b) {
+	static double add(double a, double b) {
 		return a + b;
 	}
 
-	static int subt(int a, int b) {
+	static double subt(double a, double b) {
 		return a - b;
 	}
 
+	static double mult(double a, double b) {
+		return a * b;
+	}
+
+	static double div(double a, double b) {
+		return a / b;
+	}
+
+	static double modulus(double a, double b) {
+		return a % b;
+	}
+
 	// ============ One Time Functions ============= //
+
 	static void printGreeting() {
 		println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		println("+ \t\t\t\t\t\t\t\t +");
 		println("+ \t\t Welcome to the Java Calculator \t\t +");
 		println("+ \t\t \u0020\u0020\u0020\u0020Created: 07 - 31 - 2017 \t\t\t +");
-		println("+ \t\t \u0020\u0020\u0020\u0020Updated: 07 - 31 - 2017 \t\t\t +");
+		println("+ \t\t \u0020\u0020\u0020\u0020Updated: 08 - 09 - 2017 \t\t\t +");
 		println("+ \t\t\t\t\t\t\t\t +");
 		println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		println("+ \t\t\t\t\t\t\t\t +");
